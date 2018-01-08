0fa0c3f9ebb2f16a2140660d9f3301d9e36cf26c
Faylixe <felix.voituret@gmail.com>
2018-01-08 22:09:54 +0100

    Finalizes bookler.

--- bookler.py
+++ bookler.py
@@ -10,8 +10,14 @@ from BeautifulSoup import BeautifulSoup as soup
 from requests import get
 from shutil import copyfileobj
 
-# TODO : Consider use specific logger configuration.
-logging.basicConfig(level=logging.INFO, format='[%(asctime)s][%(levelname)s] %(message)s', datefmt='%Y-%m-%d %I:%M:%S')
+#logging.basicConfig(level=logging.INFO, format='[%(asctime)s][%(levelname)s] %(message)s', datefmt='%Y-%m-%d %I:%M:%S')
+_logger = logging.getLogger('bookler')
+_logger.setLevel(logging.DEBUG)
+_formatter = logging.Formatter('%(asctime)s :: %(levelname)s :: %(message)s')
+_stream_handler = logging.StreamHandler()
+_stream_handler.setFormatter(_formatter)
+_stream_handler.setLevel(logging.DEBUG)
+_logger.addHandler(_stream_handler)
 
 def _download(url, path):
     """
@@ -19,23 +25,13 @@ def _download(url, path):
     :param path:
     :returns:
     """
-	response = get(url, stream=True)
-	if response.status_code == 200:
-		with open(path, 'wb') as output:
-			response.raw.decode_content = True
-			copyfileobj(response.raw, output)
-			return True
-	return False
-
-
-class Mode:
-    """ """
-
-    """ """
-    VOLUME = 1
-
-    """ """
-    CHAPTER = 2
+    response = get(url, stream=True)
+    if response.status_code == 200:
+        with open(path, 'wb') as output:
+            response.raw.decode_content = True
+            copyfileobj(response.raw, output)
+            return True
+    return False
 
 class Bookler(object):
     """ """
@@ -50,36 +46,46 @@ class Bookler(object):
         self._image_extractor = image_extractor
     
     def run(
+        self,
         target_directory,
-        mode=Mode.VOLUME,
-        start_parameter=(1, 1, 1),
+        start_chapter=1,
+        start_page=1,
         failure_threshold=2):
         """
         :param target_directory:
         """
         if not exists(target_directory):
             makedirs(target_directory)
-        current_volume = start_parameter[0]
-        current_chapter = start_parameter[1]
-        current_page = start_parameter[2]
+        current_chapter = start_chapter
+        current_page = start_page
+        current_directory = join(target_directory, str(current_chapter))
+        if not exists(current_directory):
+            makedirs(current_directory)
         failure = 0
-        next_page_url = self._url_builder(current_volume, current_chapter, current_page)
+        _logger.info('Downloading chapter %s' % current_chapter)
+        next_page_url = self._url_builder(current_chapter, current_page)
         while next_page_url is not None:
             response = get(next_page_url)
-	        if response.status_code == 200:
+            if response.status_code == 200:
                 html = soup(response.text)
                 image_url = self._image_extractor(html)
-                # TODO : Build path.
-                success = _download(image_url, None)
+                path = join(current_directory, str(current_page)) + '.jpg'
+                success = _download(image_url, path)
                 if success:
                     failure = 0
                 else:
+                    _logger.warn('Failed downloading page %s' % current_page)
                     failure += 1
             else:
                 failure += 1
+                _logger.warn('Failed downloading page %s' % current_page)
+            if failure == 0:
+                current_page += 1
             if failure > failure_threshold:
-                if mode == Mode.VOLUME:
-                    current_volume += 1
-                else:
-                    current_chapter += 1
-            next_page_url = self._url_builder(current_volume, current_chapter, current_page)
+                current_chapter += 1
+                current_page = start_page
+                current_directory = join(target_directory, str(current_chapter))
+                if not exists(current_directory):
+                    makedirs(current_directory)
+                _logger.info('Downloading chapter %s' % current_chapter)
+            next_page_url = self._url_builder(current_chapter, current_page)
--- sample.py
+++ sample.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python
 
-from bookler import Bookler, Mode
+from bookler import Bookler
 
-def build_url(volume, chapter, page):
+def build_url(chapter, page):
     """ """
-    return 'http://www.japscan.com/lecture-en-ligne/love-hina/volume-%s/%s.html' % (volume, page)
+    return 'http://www.japscan.com/lecture-en-ligne/love-hina/volume-%s/%s.html' % (chapter, page)
 
 def get_image_url(soup):
     """ """
     img = soup.find('img', {'id': 'image'})
-		if img is not None:
-            for attribute in img.attrs:
-                if attribute[0] == 'src':
-                    return attribute[1]
+    if img is not None:
+        for attribute in img.attrs:
+            if attribute[0] == 'src':
+                return attribute[1]
     return None
 
 if __name__ == '__main__':
     bookler = Bookler(build_url, get_image_url)
-    bookler.run('Love Hina')    
+    bookler.run('/Users/fv/Books/Love Hina')
